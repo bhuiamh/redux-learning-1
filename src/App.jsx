97e90508c302005542d9ad84e9f36af5ba72a1f4
bhuiamhe <bhuiamh@gmail.com>
2023-08-27 00:17:55 +0600

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <h1 className="text-2xl text-center bg-purple-700 ">Hello Redux</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,9 @@
+const Counter = () => {
+  return (
+    <div>
+      <h1>Hei I am coming from Counter Component</h1>
+    </div>
+  );
+};
+
+export default Counter;
